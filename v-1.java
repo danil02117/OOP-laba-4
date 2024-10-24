a4af0a9a9800f12b317b38fe8db537b7c9accee9
danil02117 <125909605+danil02117@users.noreply.github.com>
2024-10-24 07:03:33 +0600

    Create v-1.java

---
+++ v-1.java
@@ -0,0 +1,43 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;
+import java.util.Random;
+import javax.swing.*;
+
+public class main {
+    public static void main(String[] args) {
+        final ArrayList<String> strings = new ArrayList<String>();
+        strings.add("Первая строка");
+        strings.add("Вторая строка");
+        strings.add("Третья строка");
+        strings.add("Четвертая строка");
+        strings.add("Пятая строка");
+        strings.add("Шестая строка");
+        final JFrame frame = new JFrame("Движение по экрану строк");
+        frame.setPreferredSize(new Dimension(800, 500));
+        frame.setVisible(true);
+        frame.setLayout(null);
+        final JLabel lbl = new JLabel();
+        lbl.setLocation(-1, 0);
+        lbl.setSize(300, 20);
+        frame.add(lbl);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        Timer timer = new Timer(50, new ActionListener() {
+            int speedX, speedY; // скорость движения текста
+            Random rnd = new Random();
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                Point loc = lbl.getLocation();
+                if (loc.x > frame.getWidth() || loc.y > frame.getHeight() || loc.x < 0 || loc.y < 0) {
+                    lbl.setLocation(frame.getWidth()/2, frame.getHeight()/2);
+                    speedX = -5+rnd.nextInt(10);
+                    speedY = -5+rnd.nextInt(10);
+                    lbl.setText(strings.get(rnd.nextInt(strings.size()-1)));
+                } else {
+                    lbl.setLocation(loc.x + speedX, loc.y + speedY);
+                }
+            }});
+        timer.start();
+    }
+}
